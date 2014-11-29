48714e64695066c719fbe056d5970cf7aa8f8abc
Chad Burnette <chadburnette@me.com>
2014-11-28 21:34:56 -0500

    figuring out rspec and how i want to test this thing

--- spec/boxr_spec.rb
+++ spec/boxr_spec.rb
@@ -2,27 +2,47 @@ require 'spec_helper'
 
 describe Boxr do
 
+	#PLEASE NOTE: This test is intentionally NOT a series of unit tests.  The goal is to test the entire code base
+	# 						against an actual Box account, making real calls to the Box API.  The Box API is subject to frequent
+	# 						changes and it is not sufficient to mock responses as those responses will change over time.  Successfully
+	# 						running this test suite shows that the code base works with the current Box API.
+
 	#follow the directions in .env.example to set up your BOX_DEVELOPER_TOKEN
 	#keep in mind it is only valid for 60 minutes
 	BOX_CLIENT = Boxr::Client.new(ENV['BOX_DEVELOPER_TOKEN'])
 	
-	#uncomment this line to see the HTTP request and response debug info in the rspec output
+	#ncomment this line to see the HTTP request and response debug info in the rspec output
 	#BOX_CLIENT.debug_device = STDOUT
 
-	RSPEC_FOLDER_PATH = '/Boxr_RSpec'
+	TEST_FOLDER_NAME = 'Boxr_Test'
+	SUB_FOLDER_NAME = 'sub_folder_1'
 
-	it 'performs a smoke test of all Boxr functionality against a real Box account' do
-		#first step: find any pre-existing 'Boxr_RSpec' folder and delete it
-		begin
-			folder_id = BOX_CLIENT.folder_id(RSPEC_FOLDER_PATH)
-			BOX_CLIENT.delete_folder(folder_id, recursive: true)
-		rescue
+	context 'A smoke test of all Boxr functionality against a real Box account' do
+
+		it 'deletes pre-existing test folder if found and creates a new test folder' do
+			#find any pre-existing 'Boxr_RSpec' folder and delete it
+			root_folders = BOX_CLIENT.folder_items(Boxr::ROOT).folders
+			test_folder = root_folders.select{|f| f.name == TEST_FOLDER_NAME}.first
+			
+			if(test_folder)
+				BOX_CLIENT.delete_folder(test_folder.id, recursive: true)
+			end
+
+			#create a new, empty test folder
+			new_folder = BOX_CLIENT.create_folder(TEST_FOLDER_NAME, Boxr::ROOT)
+			expect(new_folder).to be_a Hashie::Mash
 		end
 
-		#now create our test folder
-		new_folder = BOX_CLIENT.create_folder(RSPEC_FOLDER_PATH, Boxr::ROOT)
-		expect(new_folder).to be_a Hashie::Mash
+		test_folder_id = nil
+		it 'looks up test folder id' do
+			test_folder_id = BOX_CLIENT.folder_id(TEST_FOLDER_NAME)
+			expect(test_folder_id).to be_a String 
+		end
 
+		it 'creates a new sub-folder' do
+			new_folder = BOX_CLIENT.create_folder(SUB_FOLDER_NAME, test_folder_id)
+			expect(new_folder).to be_a Hashie::Mash
+		end
 
 	end
 end

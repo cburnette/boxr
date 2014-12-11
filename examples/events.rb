d45d0aa9f7e75a30b87710eb1cb62abb0d7cbe12
Chad Burnette <chadburnette@me.com>
2014-12-10 21:25:52 -0500

    working on events

---
+++ examples/events.rb
@@ -0,0 +1,22 @@
+require 'dotenv'; Dotenv.load!
+require 'boxr'
+require 'awesome_print'
+require 'lru_redux'
+
+client = Boxr::Client.new(ENV['BOX_DEVELOPER_TOKEN'])
+cache = LruRedux::Cache.new(1000)
+
+stream_position = :now
+loop do 
+	puts "fetching events..."
+	event_response = client.user_events(stream_position: stream_position)
+	stream_position = event_response.next_stream_position
+	event_response.events.each do |event|
+		key = "/box-event/id/#{event.event_id}"
+		if (cache.fetch(event.event_id)==nil)
+			cache[event.event_id] = true
+			puts event.event_type
+		end
+	end
+	sleep 2
+end
--- lib/boxr/events.rb
+++ lib/boxr/events.rb
@@ -4,7 +4,7 @@ module Boxr
 		def user_events(stream_position: 0, stream_type: :all, limit: 100)
 			query = {stream_position: stream_position, stream_type: stream_type, limit: limit}
 			events, response = get(EVENTS_URI, query: query)
-			Hashie::Mash.new({entries: events["entries"], chunk_size: events["chunk_size"], next_stream_position: events["next_stream_position"]})
+			Hashie::Mash.new({events: events["entries"], chunk_size: events["chunk_size"], next_stream_position: events["next_stream_position"]})
 		end
 
 	end

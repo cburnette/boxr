2c97b2964330dd366f24dc4ccbda595e16fe6667
Chad Burnette <chadburnette@me.com>
2015-01-04 20:46:05 -0500

    working on oauth

---
+++ examples/oath.rb
@@ -0,0 +1,22 @@
+require 'dotenv'; Dotenv.load("../.env")
+require 'boxr'
+require 'uri'
+require 'awesome_print'
+
+#make sure you have BOX_CLIENT_ID and BOX_CLIENT_SECRET set in your .env file
+#make sure you have the redirect_uri for your application set to something like https://localhost:1234 in the developer portal
+
+oauth_url = Boxr::oauth_url(URI.encode_www_form_component('your-anti-forgery-token'))
+
+puts "Copy the URL below and paste into a browser. Go through the Oauth flow using the desired Box account. \
+When you are finished your browser will redirect to a 404 error page.  Look at the URL in the address bar and copy the 'code' parameter value. \
+You only have 30 seconds to complete the next step so be quick about it!"
+
+puts
+puts "URL:  #{oauth_url}"
+puts
+
+print "Enter the code: "
+code = STDIN.gets.chomp
+
+ap Boxr::get_tokens(code)
--- lib/boxr/auth.rb
+++ lib/boxr/auth.rb
@@ -8,15 +8,20 @@ module Boxr
 	end
 
 	def self.get_tokens(code, grant_type: "authorization_code", username: nil)
-		uri = "https://api.box.com/oauth2/token?code=#{code}&grant_type=#{grant_type}&client_id=#{ENV['BOX_CLIENT_ID']}&client_secret=#{ENV['BOX_CLIENT_SECRET']}"
-		uri = uri + "&username=#{username}" unless username.nil?
+		uri = "https://api.box.com/oauth2/token"
+		body = "code=#{code}&grant_type=#{grant_type}&client_id=#{ENV['BOX_CLIENT_ID']}&client_secret=#{ENV['BOX_CLIENT_SECRET']}"
+		body = body + "&username=#{username}" unless username.nil?
 
 		client = HTTPClient.new
-		client.debug_dev = STDOUT
-		res = client.post(uri)
-		puts res.status
-		puts res.body 
+		res = client.post(uri, body: body)
 
+		if(res.status==200)
+			body_json = Oj.load(res.body)
+			mash = Hashie::Mash.new(body_json)
+			return mash
+		else
+			raise BoxrException.new(status: res.status, body: res.body, header: res.header)
+		end
 	end
 
 end

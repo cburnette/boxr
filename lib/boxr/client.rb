e37ee1de5e8fb6e0fbb72be04d9bb414390cab21
Chad Burnette <chadburnette@me.com>
2015-01-05 22:43:30 -0500

    finishing work on refreshing tokens

--- lib/boxr/client.rb
+++ lib/boxr/client.rb
@@ -96,12 +96,14 @@ module Boxr
     def get_with_pagination(uri, query: {}, limit: DEFAULT_LIMIT)
       entries = []
       offset = 0
-      headers = standard_headers()
 
       begin
         query[:limit] = limit
         query[:offset] = offset
-        res = BOX_CLIENT.get(uri, query: query, header: headers)
+        res = with_auto_token_refresh do
+          headers = standard_headers()
+          BOX_CLIENT.get(uri, query: query, header: headers)
+        end
         
         if (res.status==200)
           body_json = Oj.load(res.body)
@@ -120,42 +122,53 @@ module Boxr
     def post(uri, body, query: nil, success_codes: [201], process_body: true, content_md5: nil, content_type: nil, if_match: nil)
       body = Oj.dump(body) if process_body
 
-      headers = standard_headers()
-      headers['If-Match'] = if_match unless if_match.nil?
-      headers["Content-MD5"] = content_md5 unless content_md5.nil?
-      headers["Content-Type"] = content_type unless content_type.nil?
+      res = with_auto_token_refresh do
+        headers = standard_headers()
+        headers['If-Match'] = if_match unless if_match.nil?
+        headers["Content-MD5"] = content_md5 unless content_md5.nil?
+        headers["Content-Type"] = content_type unless content_type.nil?
 
-      res = BOX_CLIENT.post(uri, body: body, query: query, header: headers)
+        BOX_CLIENT.post(uri, body: body, query: query, header: headers)
+      end
+
       check_response_status(res, success_codes)
 
       processed_response res
     end
 
     def put(uri, body, query: nil, success_codes: [200], content_type: nil, if_match: nil)
-      headers = standard_headers()
-      headers['If-Match'] = if_match unless if_match.nil?
-      headers["Content-Type"] = content_type unless content_type.nil?
+      res = with_auto_token_refresh do
+        headers = standard_headers()
+        headers['If-Match'] = if_match unless if_match.nil?
+        headers["Content-Type"] = content_type unless content_type.nil?
+        
+        BOX_CLIENT.put(uri, body: Oj.dump(body), query: query, header: headers)
+      end
 
-      res = BOX_CLIENT.put(uri, body: Oj.dump(body), query: query, header: headers)
       check_response_status(res, success_codes)
 
       processed_response res
     end
 
     def delete(uri, query: nil, success_codes: [204], if_match: nil)
-      headers = standard_headers()
-      headers['If-Match'] = if_match unless if_match.nil?
+      res = with_auto_token_refresh do
+        headers = standard_headers()
+        headers['If-Match'] = if_match unless if_match.nil?
+        
+        BOX_CLIENT.delete(uri, query: query, header: headers)
+      end
 
-      res = BOX_CLIENT.delete(uri, query: query, header: headers)
       check_response_status(res, success_codes)
 
       processed_response res
     end
 
     def options(uri, body, success_codes: [200])
-      headers = standard_headers()
+      res = with_auto_token_refresh do
+        headers = standard_headers()
+        BOX_CLIENT.options(uri, body: Oj.dump(body), header: headers)
+      end
 
-      res = BOX_CLIENT.options(uri, body: Oj.dump(body), header: headers)
       check_response_status(res, success_codes)
 
       processed_response res
@@ -174,8 +187,6 @@ module Boxr
       if res.status == 401
         auth_header = res.header['WWW-Authenticate'][0]
         if auth_header && auth_header.include?('invalid_token')
-          #refresh the tokens
-          puts 'refreshing tokens'
           new_tokens = Boxr::refresh_tokens(@refresh_token)
           @access_token = new_tokens.access_token
           @refresh_token = new_tokens.refresh_token
@@ -218,7 +229,7 @@ module Boxr
         attributes = {shared_link: {}}
       else
         attributes = {shared_link: {access: access}}
-        attributes[:shared_link][:unshared_at] = unshared_at.to_datetime.rfc3339 if unshared_at
+        attributes[:shared_link][:unshared_at] = unshared_at.to_datetime.rfc3339 unless unshared_at.nil?
         attributes[:shared_link][:permissions] = {} unless can_download.nil? && can_preview.nil?
         attributes[:shared_link][:permissions][:can_download] = can_download unless can_download.nil?
         attributes[:shared_link][:permissions][:can_preview] = can_preview unless can_preview.nil?

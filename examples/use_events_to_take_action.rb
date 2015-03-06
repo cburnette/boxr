8f7f6fa977719599a5a7baf04687c65a8ff4bc5f
Chad Burnette <chadburnette@me.com>
2015-03-06 11:55:02 -0500

    removed unneeded file

--- examples/use_events_to_take_action.rb
+++
@@ -1,8 +0,0 @@
-require 'dotenv'; Dotenv.load("../.env")
-require 'boxr'
-require 'awesome_print'
-require 'lru_redux'
-
-client = Boxr::Client.new(ENV['BOX_DEVELOPER_TOKEN'])
-cache = LruRedux::Cache.new(1000)
-

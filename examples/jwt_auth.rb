07732c07dec7d5dffdf08f29d22e5ffcd5b5bd7e
mattkim <matthewjkim@gmail.com>
2015-06-16 16:43:37 -0400

    naming parameter to prevent wrong number of arguments

--- examples/jwt_auth.rb
+++ examples/jwt_auth.rb
@@ -7,6 +7,6 @@ require 'openssl'
 private_key = OpenSSL::PKey::RSA.new(File.read(ENV['JWT_SECRET_KEY_PATH']), ENV['JWT_SECRET_KEY_PASSWORD'])
 
 #make sure ENV['BOX_ENTERPRISE_ID'] and ENV['BOX_CLIENT_ID'] are set
-response = Boxr::get_enterprise_token(private_key)
+response = Boxr::get_enterprise_token(private_key:private_key)
 
 ap response

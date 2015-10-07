ae3daf406e2c5f8d2e01777e5cd25effd4dec5d1
Chad Burnette <chadburnette@me.com>
2015-10-07 15:19:29 -0400

    Update jwt_auth.rb

--- examples/jwt_auth.rb
+++ examples/jwt_auth.rb
@@ -6,7 +6,7 @@ require 'openssl'
 
 private_key = OpenSSL::PKey::RSA.new(File.read(ENV['JWT_PRIVATE_KEY_PATH']), ENV['JWT_PRIVATE_KEY_PASSWORD'])
 
-#make sure ENV['BOX_ENTERPRISE_ID'] and ENV['BOX_CLIENT_ID'] are set
+#make sure ENV['BOX_ENTERPRISE_ID'], ENV['BOX_CLIENT_ID'] and ENV['BOX_CLIENT_SECRET'] are set
 response = Boxr::get_enterprise_token(private_key: private_key)
 
 ap response

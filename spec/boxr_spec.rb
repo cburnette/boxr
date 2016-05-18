644ffbff406b966253475983c6516a281ccbefb0
Chad Burnette <chadburnette@me.com>
2016-05-17 22:10:30 -0400

    added support for folder metadata

--- spec/boxr_spec.rb
+++ spec/boxr_spec.rb
@@ -468,8 +468,8 @@ describe Boxr::Client do
     expect(result).to eq({})
   end
 
-  #rake spec SPEC_OPTS="-e \"invokes metadata operations"\"
-  it "invokes metadata operations" do
+  #rake spec SPEC_OPTS="-e \"invokes file metadata operations"\"
+  it "invokes file metadata operations" do
     test_file = BOX_CLIENT.upload_file("./spec/test_files/#{TEST_FILE_NAME}", @test_folder)
 
     puts "create metadata"
@@ -489,6 +489,31 @@ describe Boxr::Client do
 
     puts "delete metadata"
     result = BOX_CLIENT.delete_metadata(test_file)
+    expect(result).to eq({})
+  end
+
+  #rake spec SPEC_OPTS="-e \"invokes folder metadata operations"\"
+  #NOTE: this test will fail unless you create a metadata template called 'test' with two attributes: 'a' of type text, and 'b' of type text
+  it "invokes folder metadata operations" do
+    new_folder = BOX_CLIENT.create_folder(SUB_FOLDER_NAME, @test_folder)
+
+    puts "create folder metadata"
+    meta = {"a" => "hello", "b" => "world"}
+    metadata = BOX_CLIENT.create_folder_metadata(new_folder, meta, "enterprise", "test")
+    expect(metadata.a).to eq("hello")
+
+    puts "update folder metadata"
+    metadata = BOX_CLIENT.update_folder_metadata(new_folder, {op: :replace, path: "/b", value: "there"}, "enterprise", "test")
+    expect(metadata.b).to eq("there")
+    metadata = BOX_CLIENT.update_folder_metadata(new_folder, [{op: :replace, path: "/b", value: "friend"}], "enterprise", "test")
+    expect(metadata.b).to eq("friend")
+
+    puts "get folder metadata"
+    metadata = BOX_CLIENT.folder_metadata(new_folder, "enterprise", "test")
+    expect(metadata.a).to eq("hello")
+
+    puts "delete folder metadata"
+    result = BOX_CLIENT.delete_folder_metadata(new_folder, "enterprise", "test")
     expect(result).to eq({})
   end
 

a8c345d56edeafcf193077cb949b3f597a9d0239
Zach Crownover <zachary.crownover@gmail.com>
2016-06-19 06:59:21 +0000

    Style and syntax cleanup of collaborations.rb, also noted variables that aren't used at all

--- lib/boxr/collaborations.rb
+++ lib/boxr/collaborations.rb
@@ -1,12 +1,11 @@
 module Boxr
   class Client
-
     def folder_collaborations(folder, fields: [])
       folder_id = ensure_id(folder)
       query = build_fields_query(fields, COLLABORATION_FIELDS_QUERY)
       uri = "#{FOLDERS_URI}/#{folder_id}/collaborations"
 
-      collaborations, response = get(uri, query: query)
+      collaborations, _response = get(uri, query: query)
       collaborations['entries']
     end
 
@@ -15,11 +14,11 @@ module Boxr
       query = build_fields_query(fields, COLLABORATION_FIELDS_QUERY)
       query[:notify] = :notify unless notify.nil?
 
-      attributes = {item: {id: folder_id, type: :folder}}
+      attributes = { item: { id: folder_id, type: :folder } }
       attributes[:accessible_by] = accessible_by
       attributes[:role] = validate_role(role)
 
-      collaboration, response = post(COLLABORATIONS_URI, attributes, query: query)
+      collaboration, _response = post(COLLABORATIONS_URI, attributes, query: query)
       collaboration
     end
 
@@ -30,14 +29,14 @@ module Boxr
       attributes[:role] = validate_role(role) unless role.nil?
       attributes[:status] = status unless status.nil?
 
-      updated_collaboration, response = put(uri, attributes)
+      updated_collaboration, _response = put(uri, attributes)
       updated_collaboration
     end
 
     def remove_collaboration(collaboration)
       collaboration_id = ensure_id(collaboration)
       uri = "#{COLLABORATIONS_URI}/#{collaboration_id}"
-      result, response = delete(uri)
+      result, _response = delete(uri)
       result
     end
 
@@ -48,21 +47,21 @@ module Boxr
       query = build_fields_query(fields, COLLABORATION_FIELDS_QUERY)
       query[:status] = status unless status.nil?
 
-      collaboration, response = get(uri, query: query)
+      collaboration, _response = get(uri, query: query)
       collaboration
     end
 
-    #these are pending collaborations for the current user; use the As-User Header to request for different users
+    # these are pending collaborations for the current user; use the As-User
+    # Header to request for different users
     def pending_collaborations(fields: [])
       query = build_fields_query(fields, COLLABORATION_FIELDS_QUERY)
       query[:status] = :pending
-      pending_collaborations, response = get(COLLABORATIONS_URI, query: query)
+      pending_collaborations, _response = get(COLLABORATIONS_URI, query: query)
       pending_collaborations['entries']
     end
 
-
     private
-    
+
     def validate_role(role)
       case role
       when :previewer_uploader
@@ -75,7 +74,7 @@ module Boxr
 
       role = role.to_s
       raise BoxrError.new(boxr_message: "Invalid collaboration role: '#{role}'") unless VALID_COLLABORATION_ROLES.include?(role)
-      
+
       role
     end
   end

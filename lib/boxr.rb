8f9d7c4a34be3734ef5474299edd29f6e6d8dd4c
Zach Crownover <zachary.crownover@gmail.com>
2016-06-20 04:49:51 +0000

    Style and syntax cleanup of boxr.rb

--- lib/boxr.rb
+++ lib/boxr.rb
@@ -25,44 +25,44 @@ require 'boxr/web_links'
 
 module Enumerable
   def files
-    self.select{|i| i.type == 'file'}
+    select { |i| i.type == 'file' }
   end
 
   def folders
-    self.select{|i| i.type == 'folder'}
+    select { |i| i.type == 'folder' }
   end
 
   def web_links
-    self.select{|i| i.type == 'web_link'}
+    select { |i| i.type == 'web_link' }
   end
 end
 
 class BoxrMash < Hashie::Mash
   def entries
-    self["entries"]
+    self['entries']
   end
 
   def size
-    self["size"]
+    self['size']
   end
 end
 
 module Boxr
-  Oj.default_options = {:mode => :compat }
+  Oj.default_options = { mode: :compat }
 
-  #The root folder in Box is always identified by 0
+  # The root folder in Box is always identified by 0
   ROOT = 0
 
-  #HTTPClient is high-performance, thread-safe, and supports persistent HTTPS connections
-  #http://bibwild.wordpress.com/2012/04/30/ruby-http-performance-shootout-redux/
+  # HTTPClient is high-performance, thread-safe, and supports persistent HTTPS connections
+  # http://bibwild.wordpress.com/2012/04/30/ruby-http-performance-shootout-redux/
   BOX_CLIENT = HTTPClient.new
   BOX_CLIENT.cookie_manager = nil
-  BOX_CLIENT.send_timeout = 3600 #one hour; needed for lengthy uploads
+  BOX_CLIENT.send_timeout = 3600 # one hour; needed for lengthy uploads
   BOX_CLIENT.agent_name = "Boxr/#{Boxr::VERSION}"
   BOX_CLIENT.transparent_gzip_decompression = true
-  #BOX_CLIENT.ssl_config.add_trust_ca("/Users/cburnette/code/ssh-keys/dev_root_ca.pem")
+  # BOX_CLIENT.ssl_config.add_trust_ca("/Users/cburnette/code/ssh-keys/dev_root_ca.pem")
 
-  def self.turn_on_debugging(device=STDOUT)
+  def self.turn_on_debugging(device = STDOUT)
     BOX_CLIENT.debug_dev = device
     BOX_CLIENT.transparent_gzip_decompression = false
     nil

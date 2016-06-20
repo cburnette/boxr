604eaac1f72cbc69ea9e06ae3a49ca54494ca585
Zach Crownover <zachary.crownover@gmail.com>
2016-06-20 04:22:38 +0000

    Style and syntax cleanup of shared_items.rb, marked unneeded variable.

--- lib/boxr/shared_items.rb
+++ lib/boxr/shared_items.rb
@@ -1,13 +1,11 @@
 module Boxr
   class Client
-
     def shared_item(shared_link, shared_link_password: nil)
       box_api_header = "shared_link=#{shared_link}"
       box_api_header += "&shared_link_password=#{shared_link_password}" unless shared_link_password.nil?
 
-      file_or_folder, response = get(SHARED_ITEMS_URI, box_api_header: box_api_header)
+      file_or_folder, _response = get(SHARED_ITEMS_URI, box_api_header: box_api_header)
       file_or_folder
     end
-
   end
 end

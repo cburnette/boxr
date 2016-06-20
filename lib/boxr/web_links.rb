f2f9f4118d3efbcfa95bd61e19b48d3b06716e9a
Zach Crownover <zachary.crownover@gmail.com>
2016-06-20 04:48:26 +0000

    Style and syntax cleanup of web_links.rb, marked unused variables.

--- lib/boxr/web_links.rb
+++ lib/boxr/web_links.rb
@@ -1,33 +1,29 @@
 module Boxr
   class Client
-
     def create_web_link(url, parent, name: nil, description: nil)
-
       parent_id = ensure_id(parent)
       web_link_url = verify_url(url)
-      uri = "#{WEB_LINKS_URI}"
+      uri = WEB_LINKS_URI.to_s
 
       attributes = {}
       attributes[:url] = web_link_url
-      attributes[:parent] = {:id => parent_id}
+      attributes[:parent] = { id: parent_id }
       attributes[:name] = name unless name.nil?
       attributes[:description] = description unless description.nil?
 
-      created_link, response = post(uri, attributes)
+      created_link, _response = post(uri, attributes)
       created_link
     end
 
     def get_web_link(web_link)
-
       web_link_id = ensure_id(web_link)
       uri = "#{WEB_LINKS_URI}/#{web_link_id}"
 
-      web_link, response = get(uri)
+      web_link, _response = get(uri)
       web_link
     end
 
     def update_web_link(web_link, url: nil, parent: nil, name: nil, description: nil)
-
       web_link_id = ensure_id(web_link)
       parent_id = ensure_id(parent)
       uri = "#{WEB_LINKS_URI}/#{web_link_id}"
@@ -36,27 +32,25 @@ module Boxr
       attributes[:url] = url unless url.nil?
       attributes[:name] = name unless name.nil?
       attributes[:description] = description unless description.nil?
-      attributes[:parent] = {id: parent_id} unless parent_id.nil?
+      attributes[:parent] = { id: parent_id } unless parent_id.nil?
 
-      updated_web_link, response = put(uri, attributes)
+      updated_web_link, _response = put(uri, attributes)
       updated_web_link
     end
 
     def delete_web_link(web_link)
-
       web_link_id = ensure_id(web_link)
       uri = "#{WEB_LINKS_URI}/#{web_link_id}"
 
-      result, response = delete(uri)
+      result, _response = delete(uri)
       result
     end
 
     private
 
     def verify_url(item)
-      return item if item.class == String and (item.include? 'https://' or item.include? 'http://')
+      return item if item.class == String && (item.include?('https://') || item.include?('http://'))
       raise BoxrError.new(boxr_message: "Invalid url. Must include 'http://' or 'https://'")
     end
-
   end
 end

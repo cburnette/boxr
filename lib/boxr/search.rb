ed9b3ba11eacbb2e11d44d94a1ec7f8f8436f1be
Zach Crownover <zachary.crownover@gmail.com>
2016-06-20 04:21:40 +0000

    Style and syntax cleanup of search.rb, moved comments to reduce line length, marked unneeded variables.

--- lib/boxr/search.rb
+++ lib/boxr/search.rb
@@ -1,29 +1,30 @@
 module Boxr
   class Client
+    def search(query = nil, scope: nil, file_extensions: [],
+               created_at_range_from_date: nil, created_at_range_to_date: nil,
+               updated_at_range_from_date: nil, updated_at_range_to_date: nil,
+               size_range_lower_bound_bytes: nil, size_range_upper_bound_bytes: nil,
+               owner_user_ids: [], ancestor_folder_ids: [], content_types: [], trash_content: nil,
+               mdfilters: nil, type: nil, limit: 30, offset: 0)
 
-    def search( query=nil, scope: nil, file_extensions: [], 
-                created_at_range_from_date: nil, created_at_range_to_date: nil,
-                updated_at_range_from_date: nil, updated_at_range_to_date: nil,
-                size_range_lower_bound_bytes: nil, size_range_upper_bound_bytes: nil, 
-                owner_user_ids: [], ancestor_folder_ids: [], content_types: [], trash_content: nil, 
-                mdfilters: nil, type: nil, limit: 30, offset: 0)
-
-      
       unless mdfilters.nil?
-        unless mdfilters.is_a? String   #if a string is passed in assume it is already formatted correctly
-          unless mdfilters.is_a? Array  
-            mdfilters = [mdfilters]     #if just one mdfilter is specified ensure that it is packaged inside an array
+        # if a string is passed in assume it is already formatted correctly
+        unless mdfilters.is_a? String
+          unless mdfilters.is_a? Array
+            # if just one mdfilter is specified ensure that it is packaged
+            # inside an array
+            mdfilters = [mdfilters]
           end
-          mdfilters = Oj.dump(mdfilters) 
+          mdfilters = Oj.dump(mdfilters)
         end
       end
 
-      #build range strings
+      # build range strings
       created_at_range_string = build_date_range_field(created_at_range_from_date, created_at_range_to_date)
       updated_at_range_string = build_date_range_field(updated_at_range_from_date, updated_at_range_to_date)
       size_range_string = build_size_range_field(size_range_lower_bound_bytes, size_range_upper_bound_bytes)
 
-      #build comma separated strings
+      # build comma separated strings
       file_extensions_string = to_comma_separated_string(file_extensions)
       owner_user_ids_string = to_comma_separated_string(owner_user_ids)
       ancestor_folder_ids_string = to_comma_separated_string(ancestor_folder_ids)
@@ -45,23 +46,22 @@ module Boxr
       search_query[:limit] = limit unless limit.nil?
       search_query[:offset] = offset unless offset.nil?
 
-      results, response = get(SEARCH_URI, query: search_query)
+      results, _response = get(SEARCH_URI, query: search_query)
       results.entries
     end
 
     private
 
     def build_date_range_field(from, to)
-      from_string = from.nil? ? "" : from.to_datetime.rfc3339
-      to_string = to.nil? ? "" : to.to_datetime.rfc3339
+      from_string = from.nil? ? '' : from.to_datetime.rfc3339
+      to_string = to.nil? ? '' : to.to_datetime.rfc3339
       build_range_string(from_string, to_string)
     end
 
     def build_size_range_field(lower, upper)
-      lower_string = lower.nil? ? "" : lower.to_i
-      upper_string = upper.nil? ? "" : upper.to_i
+      lower_string = lower.nil? ? '' : lower.to_i
+      upper_string = upper.nil? ? '' : upper.to_i
       build_range_string(lower_string, upper_string)
     end
-
   end
 end

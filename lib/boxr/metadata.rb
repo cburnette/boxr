41995da1b5507c5ed3a26365fce1b354cef431d1
Zach Crownover <zachary.crownover@gmail.com>
2016-06-20 04:16:11 +0000

    Style and syntax cleanup of metadata.rb, marked unneeded variables as such, fixed long lined comments, fixed quotation usage.

--- lib/boxr/metadata.rb
+++ lib/boxr/metadata.rb
@@ -1,38 +1,37 @@
 module Boxr
   class Client
-
     def create_metadata(file, metadata, scope: :global, template: :properties)
       file_id = ensure_id(file)
       uri = "#{FILE_METADATA_URI}/#{file_id}/metadata/#{scope}/#{template}"
-      metadata, response = post(uri, metadata, content_type: "application/json")
+      metadata, _response = post(uri, metadata, content_type: 'application/json')
       metadata
     end
 
     def create_folder_metadata(folder, metadata, scope, template)
       folder_id = ensure_id(folder)
       uri = "#{FOLDER_METADATA_URI}/#{folder_id}/metadata/#{scope}/#{template}"
-      metadata, response = post(uri, metadata, content_type: "application/json")
+      metadata, _response = post(uri, metadata, content_type: 'application/json')
       metadata
     end
 
     def metadata(file, scope: :global, template: :properties)
       file_id = ensure_id(file)
       uri = "#{FILE_METADATA_URI}/#{file_id}/metadata/#{scope}/#{template}"
-      metadata, response = get(uri)
+      metadata, _response = get(uri)
       metadata
     end
 
     def folder_metadata(folder, scope, template)
       folder_id = ensure_id(folder)
       uri = "#{FOLDER_METADATA_URI}/#{folder_id}/metadata/#{scope}/#{template}"
-      metadata, response = get(uri)
+      metadata, _response = get(uri)
       metadata
     end
 
     def all_metadata(file)
       file_id = ensure_id(file)
       uri = "#{FILE_METADATA_URI}/#{file_id}/metadata"
-      all_metadata, response = get(uri)
+      all_metadata, _response = get(uri)
       all_metadata
     end
 
@@ -40,10 +39,11 @@ module Boxr
       file_id = ensure_id(file)
       uri = "#{FILE_METADATA_URI}/#{file_id}/metadata/#{scope}/#{template}"
 
-      #in the event just one update is specified ensure that it is packaged inside an array
+      # in the event just one update is specified ensure that it is packaged
+      # inside an array
       updates = [updates] unless updates.is_a? Array
 
-      metadata, response = put(uri, updates, content_type: "application/json-patch+json")
+      metadata, _response = put(uri, updates, content_type: 'application/json-patch+json')
       metadata
     end
 
@@ -51,38 +51,38 @@ module Boxr
       folder_id = ensure_id(folder)
       uri = "#{FOLDER_METADATA_URI}/#{folder_id}/metadata/#{scope}/#{template}"
 
-      #in the event just one update is specified ensure that it is packaged inside an array
+      # in the event just one update is specified ensure that it is packaged
+      # inside an array
       updates = [updates] unless updates.is_a? Array
 
-      metadata, response = put(uri, updates, content_type: "application/json-patch+json")
+      metadata, _response = put(uri, updates, content_type: 'application/json-patch+json')
       metadata
     end
 
     def delete_metadata(file, scope: :global, template: :properties)
       file_id = ensure_id(file)
       uri = "#{FILE_METADATA_URI}/#{file_id}/metadata/#{scope}/#{template}"
-      result, response = delete(uri)
+      result, _response = delete(uri)
       result
     end
 
     def delete_folder_metadata(folder, scope, template)
       folder_id = ensure_id(folder)
       uri = "#{FOLDER_METADATA_URI}/#{folder_id}/metadata/#{scope}/#{template}"
-      result, response = delete(uri)
+      result, _response = delete(uri)
       result
     end
 
     def enterprise_metadata
       uri = "#{METADATA_TEMPLATES_URI}/enterprise"
-      ent_metadata, response = get(uri)
+      ent_metadata, _response = get(uri)
       ent_metadata
     end
 
     def metadata_schema(scope, template_key)
       uri = "#{METADATA_TEMPLATES_URI}/#{scope}/#{template_key}/schema"
-      schema, response = get(uri)
+      schema, _response = get(uri)
       schema
     end
-
   end
 end

f24fa55e25018ef07365d1165966cb14ad0fb5b2
Band-Aid <da-ishi@daichibox.com>
2017-02-13 15:31:50 +0900

    Create search_file.rb

---
+++ examples/search_file.rb
@@ -0,0 +1,17 @@
+
+#https://community.box.com/t5/Developer-Forum/How-to-get-results-from-Boxr-Search-results/m-p/29640#M1581
+
+require 'dotenv'; Dotenv.load("../.env")
+require 'awesome_print'
+require 'boxr'
+
+client = Boxr::Client.new(ENV['BOX_DEVELOPER_TOKEN'])
+results = client.search("test")
+
+#Print out item id for each search result
+results.each do |result|
+  puts "File Id: #{result.id}"
+end
+
+#Print out item id by the element's index number in the results array
+puts results[0].id

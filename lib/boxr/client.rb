e34ea7388725c3a1635cb4a02910da80b9915595
Band-Aid <da-ishi@daichibox.com>
2018-01-11 13:23:42 +0900

    Update client.rb

--- lib/boxr/client.rb
+++ lib/boxr/client.rb
@@ -27,7 +27,7 @@ module Boxr
     METADATA_TEMPLATES_URI = "#{API_URI}/metadata_templates"
     EVENTS_URI = "#{API_URI}/events"
     WEB_LINKS_URI = "#{API_URI}/web_links"
-    WEBHOOKS = "#{API_URI}/webhooks"
+    WEBHOOKS_URI = "#{API_URI}/webhooks"
 
 
     DEFAULT_LIMIT = 100

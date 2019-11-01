b4b9bd6d6a7a63338ec685414cc3b66589358a3e
Joao Fernandes <jfernandes@salsify.com>
2019-11-01 15:09:01 +0000

    Add specs to cover retrieving sessions and parts

--- spec/boxr/files_spec.rb
+++ spec/boxr/files_spec.rb
@@ -3,6 +3,22 @@ require 'spec_helper'
 #rake spec SPEC_OPTS="-e \"invokes file operations"\"
 describe "file operations" do
   it "invokes file operations" do
+    puts "create chunked upload session"
+    new_session = BOX_CLIENT.chunked_upload_create_session_new_file("./spec/test_files/#{TEST_LARGE_FILE_NAME}", @test_folder)
+    expect(new_session.id).not_to be_nil
+
+    puts "get chunked upload session using ID"
+    session = BOX_CLIENT.chunked_upload_get_session(new_session.id)
+    expect(session.id).to eq(new_session.id)
+
+    puts "get chunked upload parts"
+    parts = BOX_CLIENT.chunked_upload_list_parts(new_session.id)
+    expect(parts).to eq([])
+
+    puts "abort chunked upload session"
+    abort_info = BOX_CLIENT.chunked_upload_abort_session(new_session.id)
+    expect(abort_info).to eq({})
+
     puts "upload a large file in chunks"
     new_file = BOX_CLIENT.chunked_upload_file("./spec/test_files/#{TEST_LARGE_FILE_NAME}", @test_folder)
     expect(new_file.name).to eq(TEST_LARGE_FILE_NAME)
